74414e8e84ae84ad520972d827f823d9e197560f
Ivan Ordoñez <ivaneliasordonez@outlook.com>
2020-06-10 22:53:36 -0400

    solves vuetify issue in production (#36)
    * remove duplicated items by bad merge
    
    * add security tier
    
    * change pass move token controller, change tokencontroller to authcontroller
    
    * Corrige Bug al gueardar seeds
    
    * Backend CheckLists
    
    * Backend Signatures, Report COnfiguration repository, queires checklist, domain events
    
    * Uploads service & Storage helper class, paths on settings
    
    * solve typo in Shared proyect reference
    
    * Fix Bug Querying CheckLists
    
    * Ignore some local files
    
    * Optional items in  query
    
    * update dev branch  (#21)
    
    * Pipelines Build & Deploy test (#20)
    
    * remove duplicated items by bad merge
    
    * add security tier
    
    * change pass move token controller, change tokencontroller to authcontroller
    
    * Corrige Bug al gueardar seeds
    
    * Backend CheckLists
    
    * Backend Signatures, Report COnfiguration repository, queires checklist, domain events
    
    * Uploads service & Storage helper class, paths on settings
    
    * solve typo in Shared proyect reference
    
    * Fix Bug Querying CheckLists
    
    * Ignore some local files
    
    * Optional items in  query
    
    * update pipeline
    
    f
    
    * fix publish route
    
    * Update dotnet-core.yml
    
    * build path
    
    * Add test url to readme
    
    * add demo credentials
    
    * Solves Signature add and update bug
    
    * simplify api routes
    
    * Quering and DTO mappin for signatures
    
    * Backend Reports Configurations
    
    * Backend Reports Config
    
    * preparing project to deploy
    
    * updates composition api package version
    
    * change to npm
    
    * disable husky
    
    * binds to 0.0.0.0
    
    * Update dotnet-core.yml
    
    * delete web.config
    
    * dispatch pipelines
    
    * solves vuetify failling in Production Stage

--- src/ClientApp/nuxt.config.js
+++ src/ClientApp/nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'universal',
   /*
@@ -95,21 +93,8 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    optionsPath: './vuetify.options.js',
+    customVariables: ['~/assets/variables.scss']
   },
   server: {
     host: '0.0.0.0'
