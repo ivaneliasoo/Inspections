cbfaf26992bf52d0fdefd054f58b19fda66cbd67
ivan.ordonez <ivaneliasordonez@outlook.com>
2021-10-05 01:29:36 -0400

    fix: preparing to deployment

---
+++ src/ClientApp/server/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const express = require('express')
+const consola = require('consola')
+const { loadNuxt } = require('nuxt-start')
+process.env.UNIX_SOCKET = process.env.PORT
+const app = express()
+async function start () {
+  const nuxt = await loadNuxt('start')
+  await nuxt.listen(process.env.PORT, process.env.HOST)
+}
+
+start()
