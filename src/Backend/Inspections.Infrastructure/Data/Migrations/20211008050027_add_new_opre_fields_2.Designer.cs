cd3d3c33dee78c3dc1e988487ebb9b81749e488f
ivan.ordonez <ivaneliasordonez@outlook.com>
2021-10-08 01:24:22 -0400

    refactor: update api TS client

--- src/Backend/Inspections.Infrastructure/Data/Migrations/20211008050027_add_new_opre_fields_2.Designer.cs
+++ src/Backend/Inspections.Infrastructure/Data/Migrations/20211008050027_add_new_opre_fields_2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Inspections.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Inspections.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(InspectionsContext))]
-    partial class InspectionsContextModelSnapshot : ModelSnapshot
+    [Migration("20211008050027_add_new_opre_fields_2")]
+    partial class add_new_opre_fields_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
