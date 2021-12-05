7c7813b505f24b9d97bfc46578cf8d1101790abe
Kenneth Fonseca <kenneth.fonseca-ext@dominion-global.com>
2021-12-05 19:34:15 -0400

    #Create & Edit Report's Sections definition, we create repository of PrintSection

---
+++ src/Backend/Inspections.Core/Interfaces/Repositories/IPrintSectionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Inspections.Core.Domain.PrintSectionsAggregate;
+using Inspections.Shared;
+ 
+
+namespace Inspections.Core.Interfaces
+{
+    public interface IPrintSectionRepository : IAsyncRepository<PrintSection>
+    {
+    }
+}
