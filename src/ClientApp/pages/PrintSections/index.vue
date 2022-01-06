67d11f2ecf7a3db6558afe7d41f725baa9e8f7da
Kenneth Fonseca <kenneth.fonseca-ext@dominion-global.com>
2022-01-06 14:36:51 -0400

    feat: change rules in to delete  button

--- src/ClientApp/pages/PrintSections/index.vue
+++ src/ClientApp/pages/PrintSections/index.vue
@@ -1,168 +1,189 @@
 <template>
   <div>
-    <v-row>
-      <alert-dialog
-        v-model="dialogRemove"
-        title="Remove Print Section"
-        message="This operation will remove this Print Section. If Proceed, you no longer get it suggested again"
-        :code="printSection.id"
-        :description="printSection.description"
-        @yes=" deletePrintSection(printSection.id);"
-      />
-      <v-col cols="12" md="8" sm="12">
-        <v-card>
-          <v-tabs
-            color="deep-indigo accent-4"
-            left
-          >
-            <v-tab v-if="!isNew">
-              Basic Mode
-            </v-tab>
-            <v-tab>Advanced Mode</v-tab>
-            <v-tab-item v-if="!isNew">
-              <v-card height="450" style="overflow-y: scroll">
-                <draggable>
-                  <ckeditor id="myeditor" v-model="dataCKEditor" :editor="options.editor" />
-                </draggable>
-              </v-card>
-            </v-tab-item>
-            <v-tab-item>
-              <v-container fluid>
-                <v-row class="pl-4">
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="printSection.code"
-                      type="text"
-                      label="Code"
-                    />
-                  </v-col>
-                  <v-col cols="2">
-                    <v-switch v-model="printSection.isMainReport" label="Is Main Report" />
-                  </v-col>
-                </v-row>
-                <v-row class="pl-4">
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
-                  >
-                    <v-text-field
-                      v-model="printSection.description"
-                      type="text"
-                      label="Description"
-                    />
-                  </v-col>
-                </v-row>
-                <v-row class="pl-4">
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
-                  >
-                    <v-textarea
-                      v-model="dataCKEditor"
-                      type="text"
-                      label="Content"
-                    />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-tab-item>
-          </v-tabs>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="success"
-              text
-              @click="upsertPrintSection()"
+    <ValidationObserver v-slot="{ valid }" tag="form">
+      <v-row>
+        <alert-dialog
+          v-model="dialogRemove"
+          title="Remove Print Section"
+          message="This operation will remove this Print Section. If Proceed, you no longer get it suggested again"
+          :code="printSection.id"
+          :description="printSection.description"
+          @yes="deletePrintSection(printSection.id);"
+        />
+        <v-col cols="12" md="8" sm="12">
+          <v-card>
+            <v-tabs
+              v-model="selectedTab"
+              color="deep-indigo accent-4"
+              left
             >
-              Save
-            </v-btn>
-            <v-btn
-              color="default"
-              text
-            >
-              Cancel
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="12">
-        <v-card height="555" style="overflow-y: scroll">
-          <v-toolbar
-            color="indigo"
-            dark
-          >
-            <v-toolbar-title>Print Sections</v-toolbar-title>
-
-            <v-spacer />
-            <v-col cols="12" md="6" sm="12" class="mt-3">
-              <v-text-field
-                v-model="filter"
-                type="text"
-              />
-            </v-col>
-            <v-btn icon @click="getPrintSections(filter)">
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-            <v-btn icon @click="isNew=true;reset(); dataCKEditor=''">
-              <v-icon>mdi-plus</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-list two-line>
-            <v-list-item-group
-              active-class="indigo--text"
-            >
-              <draggable v-model="printSections">
-                <template v-for="(item, index) in printSections">
-                  <v-list-item :key="item.code">
-                    <v-list-item-content @click="selectPrintSection(item); dataCKEditor = item.content;isNew = false">
-                      <v-list-item-subtitle
-                        class="text--primary text-left font-weight-bold"
-                        v-text="'#' + item.code"
+              <v-tab id="basicTab" @click="activeAdvanced=false">
+                Basic Mode
+              </v-tab>
+              <v-tab id="advancedTab" @click="activeAdvanced=true">
+                Advanced Mode
+              </v-tab>
+              <v-tab-item>
+                <v-card height="450" style="overflow-y: scroll">
+                  <draggable>
+                    <ckeditor id="myeditor" v-model="dataCKEditor" :editor="options.editor" />
+                  </draggable>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-container fluid>
+                  <v-row class="pl-4">
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        rules="required"
+                        immediate
+                      >
+                        <v-text-field
+                          v-model="printSection.code"
+                          type="text"
+                          label="Code"
+                          :error-messages="errors"
+                        />
+                      </ValidationProvider>
+                    </v-col>
+                    <v-col cols="2">
+                      <v-switch v-model="printSection.isMainReport" label="Is Main Report" />
+                    </v-col>
+                  </v-row>
+                  <v-row class="pl-4">
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="12"
+                    >
+                      <ValidationProvider
+                        v-slot="{ errors }"
+                        rules="required"
+                        immediate
+                      >
+                        <v-text-field
+                          v-model="printSection.description"
+                          type="text"
+                          label="Description"
+                          :error-messages="errors"
+                        />
+                      </ValidationProvider>
+                    </v-col>
+                  </v-row>
+                  <v-row class="pl-4">
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="12"
+                    >
+                      <v-textarea
+                        v-model="dataCKEditor"
+                        type="text"
+                        label="Content"
                       />
-                      <v-list-item-subtitle class="text-left" v-text="item.description" />
-                    </v-list-item-content>
-                    <v-list-item-action>
-                      <v-list-item-action-text v-text="'Is Main Report'" />
-                      <v-icon
-                        v-if="!item.isMainReport"
-                        class="mr-6"
-                        color="grey lighten-1"
-                      >
-                        mdi-key-star
-                      </v-icon>
-                      <v-icon
-                        v-else
-                        class="mr-6"
-                        color="green darken-3"
-                      >
-                        mdi-key-star
-                      </v-icon>
-                    </v-list-item-action>
-                    <v-list-item-action>
-                      <v-list-item-action-text v-text="'Delete'" />
-                      <v-btn icon color="red" :disabled="item.isMainReport" @click=" dialogRemove = true">
-                        <v-icon>mdi-delete</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-tab-item>
+            </v-tabs>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                v-if="activeAdvanced"
+                color="success"
+                text
+                :disabled="!valid"
+                @click="upsertPrintSection()"
+              >
+                Save
+              </v-btn>
+              <v-btn
+                color="default"
+                text
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4" sm="12">
+          <v-card height="555" style="overflow-y: scroll">
+            <v-toolbar
+              color="indigo"
+              dark
+            >
+              <v-toolbar-title>Print Sections</v-toolbar-title>
 
-                  <v-divider
-                    v-if="index < printSections.length - 1"
-                    :key="item.id"
-                  />
-                </template>
-              </draggable>
-            </v-list-item-group>
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
+              <v-spacer />
+              <v-col cols="12" md="6" sm="12" class="mt-3">
+                <v-text-field
+                  v-model="filter"
+                  type="text"
+                />
+              </v-col>
+              <v-btn icon @click="getPrintSections(filter)">
+                <v-icon>mdi-magnify</v-icon>
+              </v-btn>
+              <v-btn icon @click="isNew=true;reset(); dataCKEditor='';selectedTab = 'advancedTab'">
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-list two-line>
+              <v-list-item-group
+                active-class="indigo--text"
+              >
+                <draggable v-model="printSections">
+                  <template v-for="(item, index) in printSections">
+                    <v-list-item :key="item.code">
+                      <v-list-item-content @click="selectPrintSection(item); dataCKEditor = item.content;isNew = false">
+                        <v-list-item-subtitle
+                          class="text--primary text-left font-weight-bold"
+                          v-text="'#' + item.code"
+                        />
+                        <v-list-item-subtitle class="text-left" v-text="item.description" />
+                      </v-list-item-content>
+                      <v-list-item-action>
+                        <v-list-item-action-text v-text="'Is Main Report'" />
+                        <v-icon
+                          v-if="!item.isMainReport"
+                          class="mr-6"
+                          color="grey lighten-1"
+                        >
+                          mdi-key-star
+                        </v-icon>
+                        <v-icon
+                          v-else
+                          class="mr-6"
+                          color="green darken-3"
+                        >
+                          mdi-key-star
+                        </v-icon>
+                      </v-list-item-action>
+                      <v-list-item-action>
+                        <v-list-item-action-text v-text="'Delete'" />
+                        <v-btn icon color="red" :disabled="item.isMainReport" @click=" dialogRemove = true;printSection.id=item.id">
+                          <v-icon>mdi-delete</v-icon>
+                        </v-btn>
+                      </v-list-item-action>
+                    </v-list-item>
+
+                    <v-divider
+                      v-if="index < printSections.length - 1"
+                      :key="item.id"
+                    />
+                  </template>
+                </draggable>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </ValidationObserver>
   </div>
 </template>
 
@@ -171,6 +192,7 @@ import { ref, defineComponent, reactive, computed, useStore, useFetch, watch } f
 import Vue from 'vue'
 import CKEditor from '@ckeditor/ckeditor5-vue2'
 import draggable from 'vuedraggable'
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import ClassicEditor from '~/assets/ckeditor5.0.0/ckeditor'
 import { PrintSectionState } from '~/store/printsection'
 import { PrintSectionDTO } from '@/types/PrintSections/ViewModels/PrintSectionDTO'
@@ -180,7 +202,9 @@ Vue.use(CKEditor)
 export default defineComponent({
   name: 'PrintSectionDetail',
   components: {
-    draggable
+    draggable,
+    ValidationObserver,
+    ValidationProvider
   },
   layout: undefined,
   setup () {
@@ -188,10 +212,12 @@ export default defineComponent({
     const store = useStore()
     const dialogRemove = ref<boolean>(false)
     const loading = ref<boolean>(false)
+    const activeAdvanced = ref<boolean>(false)
     const isNew = ref<boolean>(false)
+    const selectedTab = ref<string>('basicTab')
     const filter = ref<String>('')
     const dataCKEditor = ref<String>('')
-    const printSection = ref({ id: 0, code: '', description: '', content: '', isMainReport: true, status: 0 } as PrintSectionDTO)
+    const printSection = ref({ id: 0, code: '', description: '', content: '', isMainReport: false, status: 0 } as PrintSectionDTO)
     const options = reactive({
       editor: ClassicEditor
     })
@@ -217,11 +243,13 @@ export default defineComponent({
       store
         .dispatch('printsection/deletePrintSection', id, { root: true })
         .then(() => {
+          reset()
         })
     }
 
     const reset = () => {
-      printSection.value = { id: 0, code: '', description: '', content: '', isMainReport: true, status: 0 }
+      dataCKEditor.value = ''
+      printSection.value = { id: 0, code: '', description: '', content: '', isMainReport: false, status: 0 }
     }
 
     const upsertPrintSection = async () => {
@@ -254,7 +282,9 @@ export default defineComponent({
       loading,
       isNew,
       filter,
+      activeAdvanced,
       reset,
+      selectedTab,
       selectPrintSection,
       printSection,
       printSections,
