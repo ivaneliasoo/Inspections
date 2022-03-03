a99b8669be0aad8f0f32bed58de2f0974bf8278e
Carlos Arevalo <carevaloc@gmail.com>
2022-03-03 12:52:55 -0400

    - Fix bug in numeric field editing - Make item description field multiline (using text area) - Fix bug in cost sheet recalc - Complete the summary table - Add auto numbering for sections and items - When adding sections and items use the sheet's material markup as default

---
+++ src/ClientApp/components/CostSheets/Summary.vue
@@ -0,0 +1,111 @@
+<template>
+  <td colspan="7">
+    <table>
+      <thead class="header">
+        <th class="text-body2 text-left">Final Markup</th>
+        <th class="text-body2" colspan="2">5.00%</th>
+        <th class="text-body2" colspan="2">10%</th>
+        <th class="text-body2" colspan="2">15%</th>	
+      </thead>
+      <tbody class="cs-section">
+        <SummaryRow
+          :costSheet="costSheet"
+          title="Total Sales:"
+          valueMethod="totalSales"
+          percentMethod="totalSalesPercent"
+          :lowerPercent="-5"
+          :higherPercent="5"
+          :triggerUpdate="updateRow">
+        </SummaryRow>
+        <SummaryRow
+          :costSheet="costSheet"
+          title="Material Cost:"
+          valueMethod="materialCost"
+          percentMethod="materialCostPercent"
+          :lowerPercent="-5"
+          :higherPercent="5"
+          :triggerUpdate="updateRow">
+        </SummaryRow>
+        <SummaryRow
+          :costSheet="costSheet"
+          title="Labour Cost:"
+          valueMethod="labourCost"
+          percentMethod="labourCostPercent"
+          :lowerPercent="-5"
+          :higherPercent="5"
+          :triggerUpdate="updateRow">
+        </SummaryRow>
+        <SummaryRow
+          :costSheet="costSheet"
+          title="Gross Profit:"
+          valueMethod="grossProfit"
+          percentMethod="grossProfitPercent"
+          :lowerPercent="-5"
+          :higherPercent="5"
+          :highlight="true"
+          :triggerUpdate="updateRow">
+        </SummaryRow>
+      </tbody>
+    </table>
+  </td>
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+}
+
+.header {
+  border: 1px solid black;
+  background-color: #7f7f7f;
+  color: white;
+}
+
+.cs-section {
+  border: 1px solid black;
+}
+
+th {
+  background-color: #7f7f7f;
+  padding: 1px;
+}
+
+td {
+  background-color: #eae4ef;
+  padding: 1px;
+}
+
+</style>
+
+<script>
+import { Section, Item } 
+    from '../../composables/costsheets/entity.js';
+
+export default {
+  props: {
+    costSheet: Object,
+    triggerUpdate: Number
+  },
+  data: () => ({
+    selected: null,
+    updateRow: 0
+  }),
+  computed: {
+    toQuotePrice() {
+      return 0;
+    },
+    summation() {
+      return 0;
+    }
+  },
+  watch: { 
+    triggerUpdate(newVal, oldVal) {
+      this.$forceUpdate();
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
---
+++ src/ClientApp/components/CostSheets/SummaryRow.vue
@@ -0,0 +1,86 @@
+<template>
+  <tr>
+    <td style="width: 200px" class="text-body-2 text-left">
+      {{title}}
+    </td>
+    <td class="text-caption font-weight-bold title-7">
+      <NumberField
+        :value="costSheet[valueMethod](lowerPercent)" format="currency" :readOnly="true">
+      </NumberField>
+    </td>
+    <td class="text-caption font-weight-bold title-7" :style="style">
+      <NumberField
+        :value="costSheet[percentMethod](lowerPercent)" format="percent" :readOnly="true">
+      </NumberField>
+    </td>
+    <td class="text-caption font-weight-bold title-7">
+      <NumberField
+        :value="costSheet[valueMethod]()" format="currency" :bold="true" :readOnly="true">
+      </NumberField>
+    </td>
+    <td class="text-caption font-weight-bold title-7" :style="style">
+      <NumberField
+        :value="costSheet[percentMethod]()" format="percent" :bold="true" :readOnly="true">
+      </NumberField>
+    </td>
+    <td class="text-caption font-weight-bold title-7">
+      <NumberField
+        :value="costSheet[valueMethod](higherPercent)" format="currency" :readOnly="true">
+      </NumberField>
+    </td>
+    <td class="text-caption font-weight-bold title-7" :style="style">
+      <NumberField
+        :value="costSheet[percentMethod](higherPercent)" format="percent" :readOnly="true">
+      </NumberField>
+    </td>
+  </tr>
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+}
+
+td {
+  border: 1px solid black;
+}
+</style>
+
+<script>
+import { Section, Item } 
+    from '../../composables/costsheets/entity.js';
+
+export default {
+  props: {
+    costSheet: Object,
+    title: String,
+    valueMethod: String,
+    percentMethod: String,
+    lowerPercent: Number,
+    higherPercent: Number,
+    highlight: Boolean,
+    triggerUpdate: Number
+  },
+  data: () => ({
+    selected: null,
+    contextMenuVisible: false,
+    rowIndex: 0,
+    xcoord: 0,
+    ycoord: 0,
+    inactive: true
+  }),
+  watch: { 
+    triggerUpdate(newVal, oldVal) {
+      this.$forceUpdate();
+    }
+  },
+  computed: {
+    style() {
+      if (this.highlight) {
+        return "background-color: #9eedca;"
+      }
+      return "";
+    }
+  }
+};
+</script>
